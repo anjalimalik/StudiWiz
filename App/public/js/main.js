cbf96800bf31556da5ece9a673b81642d7710342
Anjali Malik <anjali30malik@gmail.com>
2018-04-27 05:31:41 -0400

    login()

--- App/public/js/main.js
+++ App/public/js/main.js
@@ -1,15 +1,52 @@
 "use strict";
 
+var urlLogin = "http://localhost:3000/login";
+
 var mylist;
 var i;
 var close;
-var list; 
+var list;
 
 // main
 
 function onload_main() {
-    mytasks_onclick();
 
+
+    mytasks_onclick();
+}
+
+function login() {
+    var email = emailLogin.value;
+    var pass = passLogin.value;
+
+    fetch(urlLogin, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            "email": email,
+            "pass": pass
+        })
+
+    }).then(function (res) {
+        if (res.ok) {
+            res.json().then(function (data) {
+                var u = 'dashboard.html?email='.concat(email);
+                window.location.href = u;
+            }.bind(this));
+        }
+        else {
+            alert("Error: Login unsuccessful!");
+            res.json().then(function (data) {
+                console.log(data.message);
+            }.bind(this));
+        }
+    }).catch(function (err) {
+        alert("Error: No internet connection!");
+        console.log(err.message + ": No Internet Connection");
+    });
 }
 
 function mytasks_onclick() {
